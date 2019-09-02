de1317f739d6e310fdd28ed17779360fc94f620f
masterUNG <email@abc.com>
2019-09-02 15:08:16 +0700

    version 1

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+     const url = req.url
+ switch (url) {
+     case '/':
+          res.end('<h1>Home Page</h1>')
+          break;
+
+     case '/about':
+          res.end('<h1>About Page</h1>')
+          braak;
+     default:
+          res.end('<h1>Page Not Found<h1></h1>')
+          break;
+ }
+
+
+})
+
+const port = process.env.PORT
+server.listen(port)
